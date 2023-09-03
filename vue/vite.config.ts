d2c6f9b6b487512e495c1b3d94b8be1be6c80679
Kaciras <Kaciras@protonmail.com>
2023-09-03 22:39:33 +0800

    visualize module size

--- vue/vite.config.ts
+++ vue/vite.config.ts
@@ -3,6 +3,7 @@ import vue from "@vitejs/plugin-vue";
 import vueSvgSfc from "vite-plugin-svg-sfc";
 import nested from "postcss-nested";
 import vars from "postcss-simple-vars";
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
 	css: {
@@ -11,6 +12,7 @@ export default defineConfig({
 		},
 	},
 	plugins: [
+		visualizer(),
 		vue(),
 		vueSvgSfc({ svgProps: attrs => delete attrs.class }),
 	],
