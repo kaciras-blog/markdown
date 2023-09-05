fa2b059ce534e9b43e6e39d62728a7bb9e7cdc17
Kaciras <Kaciras@protonmail.com>
2023-09-05 13:19:22 +0800

    Set the correct `base`

--- vue/vite.config.ts
+++ vue/vite.config.ts
@@ -1,13 +1,15 @@
-import { mergeConfig, UserConfig } from "vite";
+import { env } from "process";
+import { defineConfig, mergeConfig, UserConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueSvgSfc from "vite-plugin-svg-sfc";
 import { visualizer } from "rollup-plugin-visualizer";
 import coreConfig from "../core/vite.config.ts";
 
-export default mergeConfig(coreConfig as UserConfig,{
+export default mergeConfig(coreConfig as UserConfig, defineConfig({
+	base: env.CI ? "/markdown/" : undefined,
 	plugins: [
 		visualizer(),
 		vue(),
 		vueSvgSfc({ svgProps: attrs => delete attrs.class }),
 	],
-});
+}));
