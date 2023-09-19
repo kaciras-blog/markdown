8abccde0e968d3a4db19e2e249ea35459cea02d7
Kaciras <Kaciras@protonmail.com>
2023-09-19 12:13:32 +0800

    implement text emphasis tool buttons

--- core/src/index.ts
+++ core/src/index.ts
@@ -10,3 +10,68 @@ export { default as Collapsible } from "./collapsible.ts";
 export { default as Collect } from "./collect.ts";
 export { default as Media } from "./directive.ts";
 export { default as TOC } from "markdown-it-toc-done-right";
+
+export enum TextWrapper {
+	None = 0,
+	Italic = 1,
+	Bold = 2,
+	Code = 4,
+	StrikeThrough = 8,
+}
+
+/**
+ * 搜索一段文本所有的包装，包装是指两端具有对称字符的语法，如 **bold**, `code` 等等。
+ *
+ * @return [文本拥有的包装, 包装字符串的长度] 二元组。
+ */
+export function getWrappers(text: string): [TextWrapper, number] {
+	let wrappers = TextWrapper.None;
+	let i = 0;
+	let k = text.length - 1;
+	let repeat = 0;
+	let prev = text.charCodeAt(0);
+
+	if (k < 1) {
+		return [TextWrapper.None, 0]; // 长度不足 2 的肯定没有包装。
+	}
+
+	// 通过前一个字符和它重复的次数来断包装的类型。
+	function getWrapperType() {
+		if (prev === 126 /* ~ */ && repeat === 2) {
+			return TextWrapper.StrikeThrough;
+		}
+		if (prev === 96 /* ` */ && repeat > 0) {
+			return TextWrapper.Code;
+		}
+		if (prev !== 42 /* * */ && prev !== 95 /* _ */) {
+			return TextWrapper.None;
+		}
+		if (repeat === 1) {
+			return TextWrapper.Italic;
+		}
+		if (repeat === 2) {
+			return TextWrapper.Bold;
+		}
+		return TextWrapper.Bold | TextWrapper.Italic;
+	}
+
+	for (; i < k; i++, k--, repeat++) {
+		const c = text.charCodeAt(i);
+		if (c !== text.charCodeAt(k)) {
+			break; // 前后对称位置的字符不一样。
+		}
+		if (c !== prev) {
+			const w = getWrapperType();
+			if (w === 0) {
+				break;
+			}
+			repeat = 0;
+			prev = c;
+			wrappers |= w;
+		}
+	}
+
+	// 如果前面字符串的不是有效的包装，则要把长度回退到最后一个包装。
+	const w = getWrapperType();
+	return [wrappers | w, w === TextWrapper.None ? i - repeat : i];
+}
--- vue/src/BaseSyntaxWeights.vue
+++ vue/src/BaseSyntaxWeights.vue
@@ -1,20 +1,15 @@
 <template>
-	<ToolButton type='icon' title='粗体' @click='switchWrapper("**")'>
+	<ToolButton type='icon' title='粗体' @click='switchWrapper(TextWrapper.Bold)'>
 		<BoldIcon/>
 	</ToolButton>
-	<ToolButton type='icon' title='斜体' @click='switchWrapper("*")'>
+	<ToolButton type='icon' title='斜体' @click='switchWrapper(TextWrapper.Italic)'>
 		<ItalicIcon/>
 	</ToolButton>
-	<ToolButton type='icon' title='删除线' @click='switchWrapper("~~")'>
+	<ToolButton type='icon' title='删除线' @click='switchWrapper(TextWrapper.StrikeThrough)'>
 		<StrikethroughIcon/>
 	</ToolButton>
-	<ToolButton type='icon' title='行内代码' @click='switchWrapper("`")'>
+	<ToolButton type='icon' title='行内代码' @click='switchWrapper(TextWrapper.Code)'>
 		<CodeIcon/>
-	</KxButton>
-	<KxButton type='icon' title='横线' @click='addNewLine("- - -")'>
-		<RemoveIcon/>
-	</KxButton>
-	<KxButton type='icon' title='引用块' @click='addPrefixToLines(">")'>
 	</ToolButton>
 	<ToolButton type='icon' title='引用块' @click='addPrefixToLines(">")'>
 		<QuoteIcon/>
@@ -22,42 +17,83 @@
 </template>
 
 <script setup lang="ts">
+import { editor, Range, Selection } from "monaco-editor/esm/vs/editor/editor.api";
 import BoldIcon from "@material-design-icons/svg/round/format_bold.svg?sfc";
 import ItalicIcon from "@material-design-icons/svg/round/format_italic.svg?sfc";
 import StrikethroughIcon from "@material-design-icons/svg/round/strikethrough_s.svg?sfc";
-import CodeIcon from "@material-design-icons/svg/round/code.svg?sfc";
 import QuoteIcon from "bootstrap-icons/icons/quote.svg?sfc";
-import { editor, Range, Selection } from "monaco-editor";
+import CodeIcon from "@material-design-icons/svg/round/code.svg?sfc";
 import ToolButton from "./ToolButton.vue";
-import { AddonContext } from "./editor-addon.ts";
+import { useAddonContext } from "./addon-api.ts";
+import { getWrappers, TextWrapper } from "@kaciras/markdown-core/src/index.ts";
 
-interface TextStateGroupProps {
-	ctx: AddonContext;
+const context = useAddonContext();
+
+class WarpCommand implements editor.ICommand {
+
+	readonly range: Selection;
+	readonly wrappers: TextWrapper;
+	readonly remove: number;
+
+	addCount = 0;
+
+	constructor(range: Selection, wrappers: TextWrapper, remove: number) {
+		this.range = range;
+		this.wrappers = wrappers;
+		this.remove = remove;
+	}
+
+	computeCursorState(model: editor.ITextModel, helper: editor.ICursorStateComputerData) {
+		const { range, addCount, remove } = this;
+		const delta = addCount - remove;
+		return new Selection(
+			range.startLineNumber,
+			range.startColumn,
+			range.endLineNumber,
+			range.endColumn + delta + delta,
+		);
+	}
+
+	getEditOperations(_: editor.ITextModel, builder: editor.IEditOperationBuilder) {
+		const { range, wrappers, remove } = this;
+		const strings = [];
+
+		if ((wrappers & TextWrapper.Bold) !== 0) {
+			strings.push("**");
+		}
+		if ((wrappers & TextWrapper.Italic) !== 0) {
+			strings.push("*");
+		}
+		if ((wrappers & TextWrapper.StrikeThrough) !== 0) {
+			strings.push("~~");
+		}
+		if ((wrappers & TextWrapper.Code) !== 0) {
+			strings.push("`");
+		}
+
+		const asc = strings.join("");
+		this.addCount = asc.length;
+
+		const s = range.getStartPosition();
+		const left = Range.fromPositions(s, s.delta(0, remove));
+		builder.addEditOperation(left, asc);
+
+		const e = range.getEndPosition();
+		const right = Range.fromPositions(e, e.delta(0, -remove));
+		builder.addEditOperation(right, strings.reverse().join(""));
+	}
 }
 
-const props = defineProps<TextStateGroupProps>();
-const dialog = useDialog();
+function switchWrapper(type: TextWrapper) {
+	const range = context.selection.value;
+	const text = context.model.value.getValueInRange(range);
 
-// eslint-disable-next-line vue/no-setup-props-destructure
-const context = props.ctx;
+	const [wrappers, length] = getWrappers(text);
+	const changed = wrappers ^ type;
 
-/**
- * 获取用户选择的范围，也可以将范围扩大到整行。
- *
- * @param extend 是否扩展到整行
- * @return [number, number] 起点和终点
- */
-function getSelectedRange(extend: boolean) {
-	const { content, selection } = context;
-	let [s, e] = selection;
-	if (extend) {
-		if (s > 0) {
-			s = content.lastIndexOf("\n", s - 1) + 1;
-		}
-		e = content.indexOf("\n", e);
-		if (e === -1) e = content.length;
-	}
-	return [s, e];
+	const command = new WarpCommand(range, changed, length);
+	context.editor.executeCommand("MD.Wrap", command);
+	context.editor.focus();
 }
 
 function addPrefixToLines(prefix: string) {
@@ -74,52 +110,6 @@ function addPrefixToLines(prefix: string) {
 		}
 	}
 	text = text.substring(1);
-
-	overwrite(context, selStart, selEnd, text);
-}
-
-function addHeader(level: number) {
-	addPrefixToLines(new Array(level + 1).join("#") + " ");
-}
-
-function addNewLine(text: string) {
-	const v = context.content;
-	const index = getSelectedRange(false)[0];
-
-	if (index > 0 && v.charAt(index - 1) !== "\n") {
-		text = "\n" + text;
-	}
-	if (index < v.length && v.charAt(index) !== "\n") {
-		text += "\n";
-	}
-
-	overwrite(context, index, index, text);
-}
-
-function switchWrapper(prefix: string) {
-	const v = context.content;
-	const [selStart, selEnd] = getSelectedRange(false);
-
-	let text = v.substring(selStart, selEnd);
-	if (text.startsWith(prefix) && text.endsWith(prefix)) {
-		text = text.substring(prefix.length, text.length - prefix.length);
-	} else {
-		text = prefix + text + prefix;
-	}
-
-	overwrite(context, selStart, selEnd, text);
-}
-
-interface AddLinkData {
-	text: string;
-	href: string;
-}
-
-async function addLink() {
-	const { text, href } = await dialog.show<AddLinkData>(AddLinkDialog).confirmPromise;
-	const str = `[${text}](${href})`;
-
-	const selEnd = getSelectedRange(false)[1];
-	overwrite(context, selEnd, selEnd, str);
+	context.editor.executeCommand("MD.Prefix", command);
 }
 </script>
