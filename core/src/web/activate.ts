6b3dc668876e344c2e233cd57deee4b263880b0f
Kaciras <Kaciras@protonmail.com>
2023-09-20 14:39:17 +0800

    refactor lazy loading

--- core/src/web/activate.ts
+++ core/src/web/activate.ts
@@ -1,10 +1,12 @@
 import { activateCopyButtons } from "./fence.js";
-import { observeLazyLoad } from "./lazy-loading.js";
+import observeLazyLoad, { LazyLoadOptions } from "./lazy-loading.js";
 
 /**
  * 激活 Markdown 元素，其实就是添加各种监听。返回一个清理函数，卸载组件后记得调用哦。
+ *
+ * @return 取消监听的函数，必须在被监视的元素移除后调用，以避免内存泄漏。
  */
-export function activate(el: HTMLElement) {
+export function activate(el: HTMLElement, options: LazyLoadOptions = {}) {
 	activateCopyButtons(el);
-	return observeLazyLoad(el);
+	return observeLazyLoad(el, options);
 }
--- core/src/web/lazy-loading.ts
+++ core/src/web/lazy-loading.ts
@@ -1,17 +1,18 @@
-import { silencePromise } from "@kaciras/utilities/browser";
-
-let codecs: string;
+import { identity, silencePromise } from "@kaciras/utilities/browser";
 
 /**
- * 检测浏览器支持的新一代视频编码，新一代指在 H.264/AVC 之后，压缩率更好且受到广泛支持的。
+ * 检测浏览器支持的新一代视频编码，新一代指在 H.264 之后，压缩率更好且受到广泛支持的。
  *
  * [查看浏览器支持哪些](https://cconcolato.github.io/media-mime-support)
  * [查询字符串的选择](https://evilmartians.com/chronicles/better-web-video-with-av1-codec)
  *
  * @return 以逗号隔开的字符串，包含所支持的编码，该值会缓存下来。
  */
-function detectSupportedCodec() {
-	if (codecs) return codecs;
+function detectSupportedVideoCodecs() {
+	// 只有 iPhone 可能不支持 MediaSource，它可以解码 HEVC。
+	if (!("MediaSource" in window)) {
+		return ["hevc"];
+	}
 
 	// 待测的编码列表，靠前的更先进，也具有更高的优先级。
 	const codecMap: Array<[string, string]> = [
@@ -19,72 +20,79 @@ function detectSupportedCodec() {
 		["hevc", "video/mp4; codecs=hvc1"],
 	];
 
-	// 只有 iPhone 可能不支持 MediaSource，它可以解码 HEVC。
-	if (!("MediaSource" in window)) {
-		return "hevc";
-	}
-
 	/*
 	 * HTMLMediaElement.canPlayType() 在未安装插件的 Edge 里返回 "probably"，但实际上无法播放。
 	 * 另外也可以用 navigator.mediaCapabilities()，不过参数略复杂。
 	 */
-	return codecs = codecMap
+	return codecMap
 		.filter(c => MediaSource.isTypeSupported(c[1]))
-		.map(c => c[0]).join(",");
+		.map(c => c[0]);
 }
 
-const handlers: Record<string, (target: any, isInView: boolean) => void> ={
+export type LazyElement = HTMLMediaElement | HTMLImageElement;
 
-	IMG(element: HTMLImageElement, isInView: boolean) {
-		if (isInView && element.dataset.src) {
-			element.src = element.dataset.src;
-			delete element.dataset.src;
-		}
-	},
+export type LazyLoadOptions = IntersectionObserverInit & {
 
-	VIDEO(target: HTMLVideoElement, isInView: boolean) {
-		if (isInView && target.dataset.src) {
-			const url = new URL(target.dataset.src, location.href);
-			delete target.dataset.src;
+	/**
+	 * 触发懒加载时根据原始 URL 返回要加载的 URL，默认原样返回。
+	 *
+	 * 该函数设计为使用客户端才能获得的信息来调整资源的 URL，如检测支持的编码。
+	 *
+	 * @param src 原始的 URL。
+	 * @param type 资源的类型，是 IMG, VIDEO, AUDIO 三者之一。
+	 */
+	getURL?: (src: string, type: string) => string;
+}
 
-			url.searchParams.set("codecs", detectSupportedCodec());
-			target.src = url.toString();
+/**
+ * 对指定容器内的媒体元素（由 ./media.ts 渲染）启用懒加载，该函数只能在浏览器端调用。
+ *
+ * 【Reader View 兼容性】
+ * JS 实现的懒加载在浏览器的阅读视图里无法工作，唯一的方案是用 loading="lazy"，
+ * 但兼容性还不行，故不建议使用阅读视图浏览。
+ */
+export default function (root: HTMLElement, options: LazyLoadOptions) {
+	const { getURL = identity } = options;
+
+	function onEnter(el: LazyElement) {
+		const { dataset } = el;
+
+		if (dataset.src) {
+			el.src = getURL(dataset.src, el.tagName);
+			delete dataset.src;
 		}
 
 		/*
 		 * play() 返回 Promise 来加等待加载完成，如果元数据还未加载完就暂停会抛出异常。
-		 * 这个异常在 Chrome 里是 AbortError，Firefox 是 DomException，无法很好地跟其他情况区分。
+		 * 在 Chrome 里是 AbortError，Firefox 是 DomException，无法很好地跟其他情况区分。
 		 *
 		 * 但元数据的加载被中断是正常的，不影响下次播放，故直接屏蔽掉。
 		 *
 		 * https://developers.google.com/web/updates/2017/06/play-request-was-interrupted
 		 */
-		if (target.classList.contains("gif")) {
-			isInView ? silencePromise(target.play()) : target.pause();
+		if (el.classList.contains("gif")) {
+			silencePromise((el as HTMLVideoElement).play());
 		}
-	},
-};
-
-function callback(entries: IntersectionObserverEntry[]) {
-	for (const entry of entries) {
-		const { target, isIntersecting } = entry;
-		handlers[target.tagName](target, isIntersecting);
 	}
-}
 
-/**
- * 对指定容器元素内的媒体启用懒加载，该函数只能在浏览器端调用。
- *
- * 【Reader View 兼容性】
- * JS 实现的懒加载在浏览器的阅读视图里无法工作，唯一的方案是用 loading="lazy"，但兼容性还不行。
- * 故不建议使用阅读视图浏览本站的文章，本站的页面已经足够简洁。
- *
- * @param root 容器元素
- * @return 取消监听的函数，必须在被监视的元素移除后调用，以避免内存泄漏。
- */
-export function observeLazyLoad(root: HTMLElement) {
-	const observer = new IntersectionObserver(callback);
-	for (const e of root.querySelectorAll("img, video")) {
+	function onLeave(el: LazyElement) {
+		if (el.classList.contains("gif")) {
+			(el as HTMLVideoElement).pause();
+		}
+	}
+
+	function callback(entries: IntersectionObserverEntry[]) {
+		for (const e of entries) {
+			if (e.isIntersecting) {
+				onEnter(e.target as LazyElement);
+			} else {
+				onLeave(e.target as LazyElement);
+			}
+		}
+	}
+
+	const observer = new IntersectionObserver(callback, options);
+	for (const e of root.querySelectorAll("img,video,audio")) {
 		observer.observe(e);
 	}
 	return observer.disconnect.bind(observer);
