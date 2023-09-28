1c0a9b943f76ff8e107ed8ed9020325f1638e670
Kaciras <Kaciras@protonmail.com>
2023-09-28 16:38:42 +0800

    refactor MediaWeights.vue

--- vue/src/MediaWeights.vue
+++ vue/src/MediaWeights.vue
@@ -1,43 +1,59 @@
 <template>
-	<ToolButton title='插入图片' @click='addImage'>
+	<ToolButton title='插入图片' @click='image(context)'>
 		<IconPhoto/>
 	</ToolButton>
-	<ToolButton title='插入视频' @click='addVideo'>
+	<ToolButton title='插入视频' @click='video(context)'>
 		<IconVideo/>
 	</ToolButton>
-	<ToolButton title='插入音频' @click='addAudio'>
+	<ToolButton title='插入音频' @click='audio(context)'>
 		<IconMusic/>
 	</ToolButton>
 </template>
 
-<script setup lang="ts">
+<script lang='ts'>
 import { selectFile } from "@kaciras/utilities/browser";
-import { IconMusic, IconPhoto, IconVideo } from "@tabler/icons-vue";
-import { useAddonContext } from "./addon-api.ts";
-import ToolButton from "./ToolButton.vue";
-
-const context = useAddonContext();
+import { AddonContext } from "./addon-api.ts";
 
 function basename(name: string) {
 	const i = name.lastIndexOf(".");
 	return i === -1 ? name : name.slice(0, i);
 }
 
-async function addImage() {
+async function addImage(ctx: AddonContext) {
 	const [file] = await selectFile("image/*");
 	const url = URL.createObjectURL(file);
-	context.insertText(`![${basename(file.name)}](${url})`, true);
+	ctx.insertText(`![${basename(file.name)}](${url})`, false);
 }
 
-async function addVideo() {
+async function addVideo(ctx: AddonContext) {
 	const [file] = await selectFile("video/*");
 	const url = URL.createObjectURL(file);
-	context.insertText(`@video[](${url})`, true);
+	ctx.insertText(`@video[](${url})`, true);
 }
 
-async function addAudio() {
+async function addAudio(ctx: AddonContext) {
 	const [file] = await selectFile("image/*");
 	const url = URL.createObjectURL(file);
-	context.insertText(`@audio[${basename(file.name)}](${url})`, true);
+	ctx.insertText(`@audio[${basename(file.name)}](${url})`, true);
 }
+</script>
+
+<script setup lang="ts">
+import { IconMusic, IconPhoto, IconVideo } from "@tabler/icons-vue";
+import ToolButton from "./ToolButton.vue";
+import { useAddonContext } from "./addon-api.ts";
+
+type MediaHandler = (context: AddonContext) => void;
+
+interface MediaWeightProps {
+	image?: MediaHandler;
+	video?: MediaHandler;
+	audio?: MediaHandler;
+}
+
+const context = useAddonContext();
+
+withDefaults(defineProps<MediaWeightProps>(), {
+	image: addImage, video: addVideo, audio: addAudio,
+});
 </script>
