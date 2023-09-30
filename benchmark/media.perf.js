d19d42522c2e839c392b3292c71651a1622eaac0
Kaciras <Kaciras@protonmail.com>
2023-09-30 17:39:26 +0800

    fix benchmarks

--- benchmark/media.perf.js
+++ benchmark/media.perf.js
@@ -2,29 +2,6 @@ import { performance } from "perf_hooks";
 import { readFileSync } from "fs";
 import { Footnote, MarkdownIt, Media, TOC } from "@kaciras-blog/markdown";
 
-function counterPlugin(markdownIt: MarkdownIt) {
-	const inner = markdownIt.renderer.render;
-	markdownIt.renderer.render = function (tokens, options, env) {
-		const counter = env.counter = Object.create(null);
-		for (const { type, children } of tokens) {
-			counter[type] ??= 0;
-			counter[type] += 1;
-
-			if (type === "inline") {
-				for (const { type } of children!) {
-					counter[type] ??= 0;
-					counter[type] += 1;
-				}
-			}
-
-
-		}
-
-		return inner.call(this, tokens, options, env);
-	};
-}
-
-
 /*
  * JS 写的解析器（356.02ms）比正则（56.43ms）慢了7倍。
  * 但即便如此，单次渲染时间差也在 1ms 之内。
@@ -34,9 +11,8 @@ const markdownIt = new MarkdownIt();
 markdownIt.use(TOC);
 markdownIt.use(Media);
 markdownIt.use(Footnote);
-markdownIt.use(counterPlugin);
 
-function run(name: string, text: string) {
+function run(name, text) {
 
 	function iter() {
 		for (let i = 0; i < 1000; i++) markdownIt.render(text);
