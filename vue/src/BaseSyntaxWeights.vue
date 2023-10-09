ace528a505f2146779147a9788b08eec2b4d3451
Kaciras <Kaciras@protonmail.com>
2023-10-09 22:50:27 +0800

    add support of multiline selection for `EmphasisCommand`

--- vue/src/BaseSyntaxWeights.vue
+++ vue/src/BaseSyntaxWeights.vue
@@ -17,7 +17,7 @@
 	</ToolButton>
 </template>
 
-<script lang="ts">
+<script lang='ts'>
 import { Emphasis, getEmphasis } from "@kaciras-blog/markdown";
 import { Range, Selection } from "monaco-editor/esm/vs/editor/editor.api.js";
 import { ICommand, IEditOperationBuilder, ITextModel } from "./addon-api.ts";
@@ -25,55 +25,70 @@ import { ICommand, IEditOperationBuilder, ITextModel } from "./addon-api.ts";
 class EmphasisCommand implements ICommand {
 
 	private readonly range: Selection;
-	private readonly emphasis: Emphasis;
-	private readonly remove: number;
+	private readonly type: Emphasis;
 
-	private addCount = 0;
+	private deltaBegin!: number;
+	private deltaEnd!: number;
 
-	constructor(range: Selection, emphasis: Emphasis, remove: number) {
+	constructor(range: Selection, type: Emphasis) {
+		this.type = type;
 		this.range = range;
-		this.emphasis = emphasis;
-		this.remove = remove;
 	}
 
 	computeCursorState() {
-		const { range, addCount, remove } = this;
-		const delta = addCount - remove;
+		const { range, deltaBegin, deltaEnd } = this;
+		if (!deltaEnd) {
+			return range;
+		}
 		return new Selection(
 			range.startLineNumber,
 			range.startColumn,
 			range.endLineNumber,
-			range.endColumn + delta + delta,
+			range.endColumn + deltaEnd + deltaBegin,
 		);
 	}
 
-	getEditOperations(_: ITextModel, builder: IEditOperationBuilder) {
-		const { range, emphasis, remove } = this;
-		const strings = [];
+	getEditOperations(model: ITextModel, builder: IEditOperationBuilder) {
+		const { range, type } = this;
 
-		if ((emphasis & Emphasis.Bold) !== 0) {
-			strings.push("**");
-		}
-		if ((emphasis & Emphasis.Italic) !== 0) {
-			strings.push("*");
-		}
-		if ((emphasis & Emphasis.StrikeThrough) !== 0) {
-			strings.push("~~");
-		}
-		if ((emphasis & Emphasis.Code) !== 0) {
-			strings.push("`");
-		}
+		for (const x of splitToLines(model, range)) {
+			const text = model.getValueInRange(x);
 
-		const asc = strings.join("");
-		this.addCount = asc.length;
+			if (text.length === 0) {
+				continue; // MD 语法简单，就不支持预先插入了，实现起来容易些。
+			}
 
-		const s = range.getStartPosition();
-		const left = Range.fromPositions(s, s.delta(0, remove));
-		builder.addEditOperation(left, asc);
+			const [emphasis, remove] = getEmphasis(text);
+			const changed = emphasis ^ type;
+			const strings = [];
 
-		const e = range.getEndPosition();
-		const right = Range.fromPositions(e, e.delta(0, -remove));
-		builder.addEditOperation(right, strings.reverse().join(""));
+			if ((changed & Emphasis.Bold) !== 0) {
+				strings.push("**");
+			}
+			if ((changed & Emphasis.Italic) !== 0) {
+				strings.push("*");
+			}
+			if ((changed & Emphasis.StrikeThrough) !== 0) {
+				strings.push("~~");
+			}
+			if ((changed & Emphasis.Code) !== 0) {
+				strings.push("`");
+			}
+
+			const prefix = strings.join("");
+			const s = x.getStartPosition();
+			const left = Range.fromPositions(s, s.delta(0, remove));
+			builder.addEditOperation(left, prefix);
+
+			const e = x.getEndPosition();
+			const right = Range.fromPositions(e, e.delta(0, -remove));
+			builder.addEditOperation(right, strings.reverse().join(""));
+
+			this.deltaEnd = prefix.length - remove;
+			if (!this.deltaBegin) {
+				this.deltaBegin = this.deltaEnd;
+			}
+		}
 	}
 }
 
@@ -104,9 +119,22 @@ class PrefixCommand implements ICommand {
 		}
 	}
 }
+
+function* splitToLines(model: ITextModel, range: Range) {
+	const { startLineNumber: s, endLineNumber: e } = range;
+	if (e === s) {
+		yield range;
+		return;
+	}
+	yield new Range(s, range.startColumn, s, model.getLineLength(s) + 1);
+	for (let i = s + 1; i < e; i++) {
+		yield new Range(i, 1, i, model.getLineLength(i) + 1);
+	}
+	yield new Range(e, 1, e, range.endColumn);
+}
 </script>
 
-<script setup lang="ts">
+<script setup lang='ts'>
 import { IconBlockquote, IconBold, IconCode, IconItalic, IconStrikethrough } from "@tabler/icons-vue";
 import ToolButton from "./ToolButton.vue";
 import { useAddonContext } from "./addon-api.ts";
@@ -114,20 +142,14 @@ import { useAddonContext } from "./addon-api.ts";
 const context = useAddonContext();
 
 function toggleEmphasis(type: Emphasis) {
-	const range = context.selection.value;
-	const text = context.editor.getModel()!.getValueInRange(range);
-
-	const [emphasis, length] = getEmphasis(text);
-	const changed = emphasis ^ type;
-
-	const command = new EmphasisCommand(range, changed, length);
+	const command = new EmphasisCommand(context.selection.value, type);
 	context.editor.focus();
-	context.editor.executeCommand("md.emphasis", command);
+	context.editor.executeCommand(null, command);
 }
 
 function addPrefix(prefix: string) {
 	const command = new PrefixCommand(context.selection.value, prefix);
 	context.editor.focus();
-	context.editor.executeCommand("md.prefix", command);
+	context.editor.executeCommand(null, command);
 }
 </script>
