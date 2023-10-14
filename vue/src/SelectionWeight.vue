c42beae92d3fbf41c3a1ac0ffb413a66e1cb80b9
Kaciras <Kaciras@protonmail.com>
2023-10-14 19:12:05 +0800

    improve selection info

--- vue/src/SelectionWeight.vue
+++ vue/src/SelectionWeight.vue
@@ -1,21 +1,24 @@
 <template>
-	<span v-if='count !== 0' :class='$style.item'>
-		选择：{{ start }} - {{ start + count }} | {{ count }} 字
+	<span v-if='x.count !== 0' :class='$style.item'>
+		{{ x.sl }}:{{ x.sc }} ({{ x.start }})
+		-
+		{{ x.el }}:{{ x.ec }} ({{ x.start + x.count }})
+		|
+		{{ x.count }} 字
 	</span>
 	<span :class='$style.item'>
 		总字数：{{ context.text.value.length }}
 	</span>
 </template>
 
-<script setup lang="ts">
+<script setup lang='ts'>
 import { shallowRef, watch } from "vue";
 import { Selection } from "monaco-editor/esm/vs/editor/editor.api.js";
 import { useAddonContext } from "./addon-api.ts";
 
 const context = useAddonContext();
 
-const start = shallowRef(0);
-const count = shallowRef(0);
+const x = shallowRef({ start: 0, count: 0, sl: 0, sc: 0, el: 0, ec: 0 });
 
 // monaco-editor 将内容按行存储，选区也是，故光标的绝对位置需要计算一下。
 // https://github.com/microsoft/vscode/blob/15094ed7fded9f2de45488c13b97a8fe64a596ed/src/vs/workbench/browser/parts/editor/editorStatus.ts#L745
@@ -24,8 +27,14 @@ watch(context.selection, (selection) => {
 	const model = context.editor.getModel()!;
 
 	const offset = Selection.createWithDirection(1, 1, startLineNumber, startColumn, 0);
-	start.value = model.getCharacterCountInRange(offset);
-	count.value = model.getCharacterCountInRange(selection);
+	x.value = {
+		sl: startLineNumber,
+		sc: startColumn,
+		el: selection.endLineNumber,
+		ec: selection.endColumn,
+		start: model.getCharacterCountInRange(offset),
+		count: model.getCharacterCountInRange(selection),
+	};
 });
 </script>
 
