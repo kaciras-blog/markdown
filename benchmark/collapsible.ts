0b4e31d5a2393150214e2756caeeefe176069ed0
Kaciras <Kaciras@protonmail.com>
2024-05-14 21:07:45 +0800

    add benchmark for collapsible.ts

---
+++ benchmark/collapsible.ts
@@ -0,0 +1,23 @@
+import { readFileSync } from "fs";
+import { defineSuite } from "esbench";
+import { collapsible, MarkdownIt } from "../core/lib/index.js";
+
+const article = readFileSync("vue/demo/KFM-zh.md", "utf8");
+
+/*
+ * JS 写的解析器（356.02ms）比正则（56.43ms）慢了7倍。
+ * 但即便如此，单次渲染时间差也在 1ms 之内。
+ */
+
+const markdownIt = new MarkdownIt("zero");
+markdownIt.use(collapsible);
+
+export default defineSuite({
+	params: {
+		text: ["@gif[A [gif] video](/video/foobar.mp4)", article],
+	},
+	setup(scene) {
+		const { text } = scene.params;
+		scene.bench("Render", () => markdownIt.render(text));
+	},
+});
