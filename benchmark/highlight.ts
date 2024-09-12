4537376cf241efba2e10241d4b9ab93f7a109a90
Kaciras <Kaciras@protonmail.com>
2024-09-12 08:57:11 +0800

    add `shiki` to benchmark

--- benchmark/highlight.ts
+++ benchmark/highlight.ts
@@ -1,25 +1,26 @@
 import { defineSuite } from "esbench";
+import { codeToHtml } from "shiki";
 import { highlight } from "../core/lib/index.js";
 import vue from "../vue/src/MarkdownEditor.vue?raw";
 import yaml from "../pnpm-lock.yaml?raw";
 import ts from "../core/src/directive.ts?raw";
 
-function getLang(code: string) {
-	switch (code) {
-		case vue: return "vue";
-		case ts: return "ts";
-		case yaml: return "yaml";
-	}
-	throw new TypeError("Should not happen");
+const codeMap = { vue, yaml, ts };
+
+// 经测试 Shiki 比 hljs 慢太多，没必要切换。
+function applyShiki(code: string, lang: string) {
+	return codeToHtml(code, { lang, theme: "dracula" });
 }
 
 export default defineSuite({
 	params: {
-		code: { vue, yaml, ts },
+		lang: Object.keys(codeMap) as Array<keyof typeof codeMap>,
 	},
 	setup(scene) {
-		const { code } = scene.params;
-		const lang = getLang(code);
+		const { lang } = scene.params;
+		const code = codeMap[lang];
+
 		scene.bench("highlight.js", () => highlight(code, lang));
+		scene.benchAsync("shiki.js", () => applyShiki(code, lang));
 	},
 });
