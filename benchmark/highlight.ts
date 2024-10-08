3424954d2981c6ba798702c86b8379b717f11693
Kaciras <Kaciras@protonmail.com>
2024-10-08 11:00:14 +0800

    add `prismjs` to benchmark

--- benchmark/highlight.ts
+++ benchmark/highlight.ts
@@ -1,5 +1,8 @@
 import { defineSuite } from "esbench";
 import { codeToHtml } from "shiki";
+import Prism from "prismjs";
+import "prismjs/components/prism-yaml";
+import "prismjs/components/prism-typescript";
 import { highlight } from "../core/lib/index.js";
 import vue from "../vue/src/MarkdownEditor.vue?raw";
 import yaml from "../pnpm-lock.yaml?raw";
@@ -7,7 +10,7 @@ import ts from "../core/src/directive.ts?raw";
 
 const codeMap = { vue, yaml, ts };
 
-// 经测试 Shiki 比 hljs 慢太多，没必要切换。
+// 经测试 Shiki 比 hljs 慢太多（4x ~ 160x），没必要切换。
 function applyShiki(code: string, lang: string) {
 	return codeToHtml(code, { lang, theme: "dracula" });
 }
@@ -16,9 +19,18 @@ export default defineSuite({
 	params: {
 		lang: Object.keys(codeMap) as Array<keyof typeof codeMap>,
 	},
+	baseline: {
+		type: "Name",
+		value: "highlight.js",
+	},
 	setup(scene) {
 		const { lang } = scene.params;
 		const code = codeMap[lang];
+
+		// 这废物库还不支持 Vue，等 2.0
+		if (lang !== "vue") {
+			scene.bench("Prism", () => Prism.highlight(code, Prism.languages[lang], lang));
+		}
 
 		scene.bench("highlight.js", () => highlight(code, lang));
 		scene.benchAsync("shiki.js", () => applyShiki(code, lang));
