6cf03227089c5fa6b0a25cbd74bef59f427c8c7e
Kaciras <contact@kaciras.com>
2026-03-27 09:58:06 +0800

    update deps, TypeScript 6

--- core/test/fence.spec.ts
+++ core/test/fence.spec.ts
@@ -4,7 +4,7 @@ import { identity } from "@kaciras/utilities/node";
 import fence from "../src/web/fence.ts";
 
 const fn = vi.fn(identity);
-const md = new MarkdownIt({ highlight: fn });
+const md = new MarkdownIt({ highlight: fn } as any);
 md.use(fence);
 
 it("should work without language", () => {
